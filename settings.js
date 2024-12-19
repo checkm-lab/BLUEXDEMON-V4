21ebf58d9a52df11b73cde7d4070c26e427e3ccc
Cynthiathefirst <cynthiaolorunfemi6@gmail.com>
2024-12-19 14:02:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2348154574139"
+global.pairingNumber = "2348169546065"
  
-global.botNumber = ["2348154574139"]
+global.botNumber = ["2348169546065"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
